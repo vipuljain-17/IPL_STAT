5358a169a01afc09d5f9ad8d0e2ed91c6565241c
Vipul Jain <vipul.today@gmail.com>
2021-04-21 15:38:47 +0530

    Frontend added to Polling Page

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, flash, url_for, redirect
+from model import db,PlayerModel
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 import pandas as pd
 
@@ -35,6 +40,28 @@ def hello_world():
 @app.route('/players/<playername>')
 def show_user_profile(playername):
 	return render_template('profile.html', player_name = playername, stats=return_stats(playername))
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/data/create' , methods = ['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('createpage.html')
+ 
+    if request.method == 'POST':
+        fav_player_name = request.form['fav_player_name']
+        predicted_team_for_cup = request.form['predicted_team_for_cup']
+        player = PlayerModel(fav_player_name= fav_player_name,predicted_team_for_cup= predicted_team_for_cup)
+        db.session.add(player)
+        db.session.commit()
+        return redirect('/data')
+
+@app.route('/data')
+def show_all():
+    players = PlayerModel.query.all()
+    return render_template('show_all.html',players = players)
 
 if __name__ == '__main__':
 	app.run(debug=True)
